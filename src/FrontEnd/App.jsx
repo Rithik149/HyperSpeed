b0c9d34a85cf81bb3c706ccbbe15514adae5dd29
Rithik Puthiyedath <60679530+Rithik149@users.noreply.github.com>
2025-12-12 00:01:45 +0530

    the alignment is done

--- src/FrontEnd/App.jsx
+++ src/FrontEnd/App.jsx
@@ -13,34 +13,35 @@ export default function App() {
         </div>
         <div className="main-container">
           <form action="/upload" method="POST">
-              <div className="container">
-                <div className="folder">
-                  <div className="front-side">
-                    <div className="tip"></div>
-                    <div className="cover"></div>
-                  </div>
-                  <div className="back-side cover"></div>
+            <div className="container">
+              <div className="folder">
+                <div className="front-side">
+                  <div className="tip"></div>
+                  <div className="cover"></div>
                 </div>
-                <label className="custom-file-upload">
-                  <input className="title" type="file" />
-                  Upload a File
-                </label>
+                <div className="back-side cover"></div>
               </div>
+              <label className="custom-file-upload">
+                <input className="title" type="file" />
+                Upload a File
+              </label>
+            </div>
           </form>
           <form action="/download" method="POST">
-            <div className="key-container">
-              <div className="key">
-                <label htmlFor="key">Receive</label>
-                <input
-                  type="text"
-                  id="key"
-                  name="code"
-                  pattern="\d{6}"
-                  maxLength="6"
-                  placeholder="Enter key"
-                  title="Enter key"
-                ></input>
-              </div>
+            <div className="key">
+              <label htmlFor="key">Receive</label>
+              <input
+                type="text"
+                id="key"
+                name="code"
+                pattern="\d{6}"
+                maxLength="6"
+                placeholder="Enter key"
+                title="Enter key"
+              ></input>
+              <button className="button" type="submit">
+                <span className="button-content">Download </span>
+              </button>
             </div>
           </form>
         </div>
