509c26d4f9419784fe5fed95087ea0046f960139
Rithik Puthiyedath <60679530+Rithik149@users.noreply.github.com>
2025-12-22 00:17:01 +0530

    Added the timer

--- src/FrontEnd/App.jsx
+++ src/FrontEnd/App.jsx
@@ -7,8 +7,25 @@ export default function App() {
   const formRef = useRef(null);
   const fileRef = useRef(null);
   const [status, setStatus] = useState("");
-  const [uploadResult, setUploadResult] = userState(null);
-  const [timeLeft, setTimeLeft] = userState(0);
+  const [uploadResult, setUploadResult] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(0);
+
+      useEffect(() => {
+      //sets timer only after upload
+      if (!uploadResult) return;
+
+      const interval = setInterval(() => {
+        setTimeLeft((prev) => {
+          if (prev <= 1) {
+            clearInterval(interval);
+            return 0;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }, [uploadResult]);
 
   async function uploadFile(file) {
     setStatus("Uploading...");
@@ -29,8 +46,6 @@ export default function App() {
 
       setUploadResult(data);
       setTimeLeft(10 * 60);
-      
-
       setStatus(`Uploaded: ${data.file.name}\nShare Code: ${data.code}`);
       console.log(status);
     } catch (err) {
@@ -48,31 +63,51 @@ export default function App() {
           <h1>Hyperspeed</h1>
         </div>
         <div className="main-container">
-          <form ref={formRef}>
-            <div className="container">
-              <div className="folder">
-                <div className="front-side">
-                  <div className="tip"></div>
-                  <div className="cover"></div>
+          {!uploadResult && (
+            <form ref={formRef}>
+              <div className="container">
+                <div className="folder">
+                  <div className="front-side">
+                    <div className="tip"></div>
+                    <div className="cover"></div>
+                  </div>
+                  <div className="back-side cover"></div>
                 </div>
-                <div className="back-side cover"></div>
+                <label className="custom-file-upload">
+                  <input
+                    className="title"
+                    type="file"
+                    name="file"
+                    ref={fileRef}
+                    accept="image/*,video/*,audio/*,.zip"
+                    onChange={(e) => {
+                      const file = e.target.files[0];
+                      if (file) uploadFile(file);
+                    }}
+                  />
+                  Upload a File
+                </label>
               </div>
-              <label className="custom-file-upload">
-                <input
-                  className="title"
-                  type="file"
-                  name="file"
-                  ref={fileRef}
-                  accept="image/*,video/*,audio/*,.zip"
-                  onChange={(e) => {
-                    const file = e.target.files[0];
-                    if (file) uploadFile(file);
-                  }}
-                />
-                Upload a File
-              </label>
+            </form>
+          )}
+          {uploadResult && (
+            <div className="share-box">
+              <p>Share this code</p>
+
+              <h1 className="share-code">{uploadResult.code}</h1>
+
+              <p className="timer">
+                Expires in: {Math.floor(timeLeft / 60)}:
+                {(timeLeft % 60).toString().padStart(2, "0")}
+              </p>
+
+              <p className="file-info">
+                {uploadResult.file.name} <br />
+                {Math.round(uploadResult.file.size / 1024)} KB
+              </p>
             </div>
-          </form>
+          )}
+
           <form action="/download" method="POST">
             <div className="key">
               <label htmlFor="key">Receive</label>
